2b0b59221a409039e3f611f8289b7a4dac6e8c57
Lj.liujun <395822362@qq.com>
2020-03-18 23:00:55 +0800

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.vimrc
+
 ln -sf ~/.dotfiles/.vim/coc-settings.json ~/.vim/coc-settings.json
