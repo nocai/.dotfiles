9d2e7b8db43bbba27a5b36da8f956756f2f3281e
liujun <liujun02@innotechx.com>
2020-03-19 09:56:37 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.vimrc
+ln -sf ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
 
 ln -sf ~/.dotfiles/.vim/coc-settings.json ~/.vim/coc-settings.json
