0f28475d6801252ec86ae9bb3d19da106c82a7cc
liujun <liujun02@innotechx.com>
2020-03-19 10:17:51 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
 
 ln -sf ~/.dotfiles/.vim/coc-settings.json ~/.vim/coc-settings.json
+ln -sf ~/.dotfiles/.vim/coc-settings.json ~/.config/nvim/coc-settings.json
