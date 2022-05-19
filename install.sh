728f03976a0bb7b70572ddffac1dda6029d942a3
nocai <395822362@qq.com>
2022-05-19 16:00:21 +0800

    init

--- install.sh
+++
@@ -1,8 +0,0 @@
-ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
-
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
-ln -sf ~/.dotfiles/.vimrc ~/.ideavimrc
-
-ln -sf ~/.dotfiles/.vim/coc-settings.json ~/.vim/coc-settings.json
-ln -sf ~/.dotfiles/.vim/coc-settings.json ~/.config/nvim/coc-settings.json
