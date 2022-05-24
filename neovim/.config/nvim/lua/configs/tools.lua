583ccf1fa69f60060b9fa957ddb2950c72959158
nocai <395822362@qq.com>
2022-05-24 19:50:15 +0800

    tools.lua

--- neovim/.config/nvim/lua/configs/tools.lua
+++ neovim/.config/nvim/lua/configs/tools.lua
@@ -3,29 +3,50 @@ local tools = {}
 function tools.gitsigns()
 	require("gitsigns").setup({
 		current_line_blame = true,
-		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
+		preview_config = {
+			-- Options passed to nvim_open_win
+			border = "rounded",
 		},
-		keymaps = {
-			noremap = true,
-			["n ]h"] = { expr = true, "&diff ? ']h' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'" },
-			["n [h"] = { expr = true, "&diff ? '[h' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'" },
-			["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-			["v <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-			["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-			["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-			["v <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-			["n <leader>hR"] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-			["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-			["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
-			-- Text objects
-			["o lh"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-			["x lh"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-		},
+		on_attach = function(bufnr)
+			local gs = package.loaded.gitsigns
+
+			local function map(mode, l, r, opts)
+				opts = opts or {}
+				opts.buffer = bufnr
+				vim.keymap.set(mode, l, r, opts)
+			end
+
+			-- stylua: ignore start
+			-- Navigation
+			map('n', ']c', function()
+				if vim.wo.diff then return ']c' end
+				vim.schedule(function() gs.next_hunk() end)
+				return '<Ignore>'
+			end, {expr=true})
+
+			map('n', '[c', function()
+				if vim.wo.diff then return '[c' end
+				vim.schedule(function() gs.prev_hunk() end)
+				return '<Ignore>'
+			end, {expr=true})
+
+			-- Actions
+			map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+			map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+			map('n', '<leader>hS', gs.stage_buffer)
+			map('n', '<leader>hu', gs.undo_stage_hunk)
+			map('n', '<leader>hR', gs.reset_buffer)
+			map('n', '<leader>hp', gs.preview_hunk)
+			map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+			map('n', '<leader>tb', gs.toggle_current_line_blame)
+			map('n', '<leader>hd', gs.diffthis)
+			map('n', '<leader>hD', function() gs.diffthis('~') end)
+			map('n', '<leader>td', gs.toggle_deleted)
+
+			-- Text object
+			map({'o', 'x'}, 'lh', ':<C-U>Gitsigns select_hunk<CR>')
+			-- stylua: ignore end
+		end,
 	})
 end
 
