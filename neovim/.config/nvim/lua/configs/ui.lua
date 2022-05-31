5b042f7377f04ec3a12c24634a391877d3d12b2e
nocai <395822362@qq.com>
2022-05-31 16:55:41 +0800

    config ui: lualine

--- neovim/.config/nvim/lua/configs/ui.lua
+++ neovim/.config/nvim/lua/configs/ui.lua
@@ -93,6 +93,11 @@ function ui.lualine()
 			theme = "auto",
 			globalstatus = true,
 		},
+		sections = {
+			lualine_a = { { "mode", separator = { left = "", right = "" } } },
+			lualine_y = { { "progress", separator = { left = "", right = "" } } },
+			lualine_z = { { "location", separator = { left = "", right = "" } } },
+		},
 		tabline = {
 			lualine_a = {
 				{
