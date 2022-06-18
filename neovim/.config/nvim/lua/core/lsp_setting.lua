f3859f372f7141676066d52a52958508db0fa0a8
刘军 <liujun@gw.com.cn>
2022-06-18 13:08:21 +0800

    keymapping: signature help

--- neovim/.config/nvim/lua/core/lsp_setting.lua
+++ neovim/.config/nvim/lua/core/lsp_setting.lua
@@ -70,7 +70,7 @@ local function on_attach(client, bufnr)
 	local opts = { buffer = bufnr }
 	-- mapping: K => E
 	vim.keymap.set("n", "E", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	vim.keymap.set({ "n", "i" }, "<C-e>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	vim.keymap.set({ "n", "i", "s" }, "<C-e>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 
 	vim.keymap.set("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
 	vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
