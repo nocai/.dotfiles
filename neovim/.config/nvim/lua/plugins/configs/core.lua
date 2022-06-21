1bb9d19997d2f61daf4cc0caebdaee37cc422f67
= <395822362@qq.com>
2022-06-22 06:05:16 +0800

    auto close nvim-tree at last window

--- neovim/.config/nvim/lua/plugins/configs/core.lua
+++ neovim/.config/nvim/lua/plugins/configs/core.lua
@@ -93,7 +93,14 @@ function M.nvim_tree()
 
 	vim.keymap.set("n", "<leader><leader>", "<cmd>NvimTreeFindFileToggle<CR>")
 	-- auto close last windows in the tab
-	vim.cmd([[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]])
+	vim.api.nvim_create_autocmd("BufEnter", {
+		nested = true,
+		callback = function()
+			if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+				vim.cmd("quit")
+			end
+		end,
+	})
 end
 
 function M.lualine()
