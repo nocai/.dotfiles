7e2de2a093ae7913796e9d01a684c9db33836b9e
刘军 <liujun@gw.com.cn>
2022-07-07 15:10:32 +0800

    (nvim) cmp.lua

--- neovim/.config/nvim/lua/plugins/configs/cmp.lua
+++ neovim/.config/nvim/lua/plugins/configs/cmp.lua
@@ -33,7 +33,7 @@ cmp.setup({
 	},
 	formatting = {
 		fields = { "kind", "abbr", "menu" },
-		format = function(_, vim_item)
+		format = function(entry, vim_item)
 			vim_item.kind = lspkind_icon[vim_item.kind] .. " "
 
 			local maxwidth = 30
@@ -41,7 +41,7 @@ cmp.setup({
 				vim_item.abbr = string.sub(vim_item.abbr, 1, maxwidth) .. "…"
 			end
 
-			-- vim_item.menu = entry.source.name
+			vim_item.menu = entry.source.name
 			vim_item.menu = ""
 
 			return vim_item
@@ -79,11 +79,11 @@ cmp.setup({
 		end, { "i", "s" }),
 	},
 	sources = cmp.config.sources({
-		{ name = "nvim_lua" },
 		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
+		{ name = "nvim_lua" },
 		{ name = "buffer" },
 		{ name = "path" },
+		{ name = "luasnip" },
 	}),
 	experimental = {
 		ghost_text = true,
