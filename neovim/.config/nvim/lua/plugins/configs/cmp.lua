1bef94e9272ef122639ae6d106056a65330eaa7c
刘军 <liujun@gw.com.cn>
2022-10-20 12:43:53 +0800

    (nvim) cmp.lua

--- neovim/.config/nvim/lua/plugins/configs/cmp.lua
+++ neovim/.config/nvim/lua/plugins/configs/cmp.lua
@@ -91,8 +91,9 @@ cmp.setup({
 		end, { "i", "s" }),
 	},
 	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
+		{ name = "nvim_lsp" },
+	}, {
 		{ name = "buffer" },
 		{ name = "path" },
 		-- { name = "nvim_lua" },
