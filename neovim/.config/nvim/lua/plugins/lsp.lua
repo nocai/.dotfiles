80255b5f0c456756ec1995e3fe82bef4247599c6
刘军 <liujun@gw.com.cn>
2023-03-01 15:26:55 +0800

    (nvim) lsp config

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -16,54 +16,60 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       {
-        "hrsh7th/cmp-nvim-lsp",
-        cond = function()
-          return require("config.lazy.util").has("nvim-cmp")
-        end,
-      },
-      {
         "williamboman/mason-lspconfig.nvim",
-        opts = function()
-          local ensure_installed = { "lua_ls" }
+        opts = {
+          ensure_installed = { "lua_ls" },
+        },
+        config = function(_, opts)
           local Servers = require("plugins.lsp.servers")
-          return {
-            ensure_installed = vim.list_extend(ensure_installed, vim.tbl_keys(Servers)),
-          }
+          require("mason-lspconfig").setup({
+            ensure_installed = vim.list_extend(opts.ensure_installed, vim.tbl_keys(Servers)),
+            automatic_installation = true,
+          })
+
+          -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+          local capabilities = vim.lsp.protocol.make_client_capabilities()
+          capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+          require("mason-lspconfig").setup_handlers({
+            function(server_name)
+              local settings = Servers[server_name] or {}
+              settings.capabilities = capabilities
+              require("lspconfig")[server_name].setup(settings)
+            end,
+          })
         end,
         dependencies = {
-          "williamboman/mason.nvim",
-          opts = {
-            ensure_installed = { "stylua" },
-            ui = { border = "rounded" },
-            github = {
-              -- download_url_template = ivim.git_proxy("https://github.com/%s/releases/download/%s/%s"),
-              download_url_template = "https://ghproxy.com/https://github.com/%s/releases/download/%s/%s",
-            },
+          {
+            "hrsh7th/cmp-nvim-lsp",
+            cond = function()
+              return require("config.lazy.util").has("nvim-cmp")
+            end,
           },
-          config = function(_, opts)
-            require("mason").setup(opts)
-            local mr = require("mason-registry")
-            for _, tool in ipairs(opts.ensure_installed) do
-              local p = mr.get_package(tool)
-              if not p:is_installed() then
-                p:install()
+          {
+            "williamboman/mason.nvim",
+            opts = {
+              ensure_installed = { "stylua" },
+              ui = { border = "rounded" },
+              github = {
+                -- download_url_template = ivim.git_proxy("https://github.com/%s/releases/download/%s/%s"),
+                download_url_template = "https://ghproxy.com/https://github.com/%s/releases/download/%s/%s",
+              },
+            },
+            config = function(_, opts)
+              require("mason").setup(opts)
+              local mr = require("mason-registry")
+              for _, tool in ipairs(opts.ensure_installed) do
+                local p = mr.get_package(tool)
+                if not p:is_installed() then
+                  p:install()
+                end
               end
-            end
-          end,
+            end,
+          },
         },
       },
     },
     config = function()
-      -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-      local Servers = require("plugins.lsp.servers")
-      for server, opts in pairs(Servers) do
-        opts.capabilities = capabilities
-        require("lspconfig")[server].setup(opts)
-      end
-
       local Util = require("plugins.lsp.util")
       Util.on_attach(function(client, buffer)
         require("plugins.lsp.setting")
