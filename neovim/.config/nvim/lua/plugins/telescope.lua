d88078207dd59cd82f36e5b94b78373d0992e7e2
刘军 <liujun@gw.com.cn>
2023-07-27 09:52:37 +0800

    (nvim) modify: telescope.lua

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -13,17 +13,18 @@ return {
     },
   },
   keys = {
-    { "<leader><space>", "<cmd>Telescope buffers<cr>", desc = "[F]ind [B]uffers" },
+    { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
 
-    { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep (telescope)" },
-    { "<leader>?", "<cmd>Telescope help_tags<cr>", desc = "Help Pages (telescope)" },
-    { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History (telescope)" },
+    { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+    { "<leader>?", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+    { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
 
     { "<leader>.", "<cmd>Telescope resume<cr>", desc = "[.]Resume" },
     { "<leader>'", "<cmd>Telescope marks<cr>", desc = "[']Marks" },
     { '<leader>"', "<cmd>Telescope<cr>", desc = '["]Builtin' },
 
     -- find
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "[F]ind [B]uffers" },
     { "<leader>fc", "<cmd>Telescope commands<cr>", desc = "Commands (telescope)" },
     { "<leader>fC", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands (telescope)" },
     { "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics (telescope)" },
@@ -31,7 +32,7 @@ return {
     { "<leader>fh", "<cmd>Telescope highlights<cr>", desc = "Highlight Groups (telescope)" },
     { "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps (telescope)" },
     { "<leader>fm", "<cmd>Telescope man_pages<cr>", desc = "Man Pages (telescope)" },
-    { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent (telescope)" },
+    { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files" },
     { "<leader>fo", "<cmd>Telescope vim_options<cr>", desc = "Options (telescope)" },
     { "<leader>fw", "<cmd>Telescope grep_string<cr>", desc = "Word (telescope)" },
 
