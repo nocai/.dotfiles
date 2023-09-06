b80870b50ca3923030405bfb81b3b3ac82502e94
刘军 <liujun@gw.com.cn>
2023-09-06 10:35:42 +0800

    (nvim) lazygit keymaping

--- neovim/.config/nvim/lua/plugins/toggleterm.lua
+++ neovim/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,22 +1,51 @@
 return {
   "akinsho/toggleterm.nvim",
   enabled = not vim.g.vscode,
-    -- stylua: ignore
-    keys = {
-      { [[<C-_>]], [[<Cmd>execute v:count . "ToggleTerm direction=horizontal"<CR>]],
-        mode = { "n", "i", "t", "x" }, desc = "Open terminal horizontal",
+  -- keys = {
+  --   { [[<C-_>]], [[<Cmd>execute v:count . "ToggleTerm direction=horizontal"<CR>]],
+  --     mode = { "n", "i", "t", "x" }, desc = "Open terminal horizontal",
+  --   },
+  --   {
+  --     [[<C-\>]], [[<Cmd>execute v:count . "ToggleTerm direction=float"<CR>]],
+  --     mode = { "n", "i", "t", "x" }, desc = "Open terminal vertical",
+  --   },
+  -- },
+  keys = function()
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      dir = "git_dir",
+      direction = "float",
+      float_opts = {
+        border = "rounded",
       },
+      hidden = true,
+      on_open = function(term)
+        vim.keymap.del({ "t" }, "<Esc>")
+      end,
+      on_close = function(term)
+        vim.keymap.set("t", "<esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
+      end,
+    })
+
+    return {
+      { [[<C-\>]] },
       {
-        [[<C-\>]], [[<Cmd>execute v:count . "ToggleTerm direction=float"<CR>]],
-        mode = { "n", "i", "t", "x" }, desc = "Open terminal vertical",
+        "<leader>G",
+        function()
+          lazygit:toggle()
+        end,
+        desc = "Lazygit",
       },
-    },
+    }
+  end,
+  cmd = { "ToggleTerm", "TermExec" },
   opts = {
-    open_mapping = false, -- [[<C-\>]]
-    start_in_insert = true,
+    open_mapping = [[<C-\>]],
     auto_scroll = false,
+    direction = "float",
     float_opts = {
-      border = "curved",
+      border = "rounded",
     },
     highlights = {
       FloatBorder = {
