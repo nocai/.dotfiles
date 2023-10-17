939faa4265f79f5ed40f9dc2e31d39b641fbe245
刘军 <liujun@gw.com.cn>
2023-10-17 19:08:55 +0800

    nvim: toggleterm.lua

--- neovim/.config/nvim/lua/plugins/toggleterm.lua
+++ neovim/.config/nvim/lua/plugins/toggleterm.lua
@@ -2,28 +2,27 @@ return {
   "akinsho/toggleterm.nvim",
   enabled = not vim.g.vscode,
   keys = function()
-    local Terminal = require("toggleterm.terminal").Terminal
-    local lazygit = Terminal:new({
-      cmd = "lazygit",
-      dir = "git_dir",
-      direction = "float",
-      float_opts = {
-        border = "rounded",
-      },
-      hidden = true,
-      on_open = function()
-        vim.keymap.del({ "t" }, "<Esc>")
-      end,
-      on_close = function()
-        vim.keymap.set("t", "<esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
-      end,
-    })
-
     return {
       { [[<C-\>]] },
       {
         "<leader>G",
         function()
+          local Terminal = require("toggleterm.terminal").Terminal
+          local lazygit = Terminal:new({
+            cmd = "lazygit",
+            dir = "git_dir",
+            direction = "float",
+            float_opts = {
+              border = "rounded",
+            },
+            hidden = true,
+            on_open = function()
+              vim.keymap.del({ "t" }, "<Esc>")
+            end,
+            on_close = function()
+              vim.keymap.set("t", "<esc>", "<c-\\><c-n>")
+            end,
+          })
           lazygit:toggle()
         end,
         desc = "Lazygit",
