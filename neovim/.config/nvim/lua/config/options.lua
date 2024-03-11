9cc04bfdd2c59cc1c06602e97453fe2bfd86c27d
刘军 <liujun@gw.com.cn>
2024-03-11 10:37:53 +0800

    (nvim) options

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -4,64 +4,50 @@ vim.g.maplocalleader = " "
 
 local opt = vim.opt
 
-opt.laststatus = 3 -- global statusline
-opt.showmode = false
-
-opt.clipboard = "unnamedplus"
-opt.cursorline = true
-
--- Indenting
-opt.expandtab = true
-opt.smartindent = true
-opt.preserveindent = true -- preserve indent structure as much as possible
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.softtabstop = 2
-
--- opt.fillchars = { eob = " " } -- disable `~` on nonexistent lines
-opt.ignorecase = true
-opt.smartcase = true
-opt.grepprg = "rg --vimgrep"
 opt.mouse = "a"
-
+opt.showmode = false
+opt.grepprg = "rg --vimgrep"
+opt.clipboard = "unnamedplus"
 -- Numbers
 opt.number = true
 opt.relativenumber = true
--- opt.numberwidth = 2
-opt.ruler = false
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
 
 -- disable nvim intro
 opt.shortmess:append("sI")
+
+opt.cursorline = true
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.scrolloff = 5 -- Lines of context
+opt.sidescrolloff = 10 -- Columns of context
 
 opt.signcolumn = "yes"
 opt.splitbelow = true
 opt.splitright = true
 opt.termguicolors = true
-opt.timeoutlen = 400
 
-opt.wrap = false -- Disable line wrap
-opt.swapfile = false
-opt.undofile = true
-
--- interval for writing swap file to disk, also used by gitsigns
 opt.updatetime = 250
-
--- go to previous/next line with h,l,left arrow and right arrow
--- when cursor reaches end/beginning of line
-opt.whichwrap:append("<>[]hl")
-
-opt.completeopt = "menu,menuone,noselect"
+opt.timeoutlen = 300
 
 opt.pumheight = 10 -- Maximum number of entries in a popup
-opt.scrolloff = 5 -- Lines of context
-opt.sidescrolloff = 10 -- Columns of context
 
-if vim.fn.has("nvim-0.10") == 1 then
-  opt.smoothscroll = true
-end
+opt.undofile = true
 
 -- Disable providers
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
+
+if vim.fn.has("nvim-0.10") == 1 then
+  opt.smoothscroll = true
+end
