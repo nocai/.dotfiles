ce146b45078abc82884bea40e4ddb3db3a03158a
刘军 <liujun@gw.com.cn>
2024-03-12 19:10:20 +0800

    (nvim) debug mapping

--- neovim/.config/nvim/lua/plugins/coding/dap.lua
+++ neovim/.config/nvim/lua/plugins/coding/dap.lua
@@ -31,24 +31,27 @@ return {
     -- },
     -- stylua: ignore
     keys = {
-      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
       { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
-      { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
-      { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
-      { "<leader>dj", function() require("dap").down() end, desc = "Down" },
-      { "<leader>dk", function() require("dap").up() end, desc = "Up" },
-      { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
-      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
-      { "<leader>ds", function() require("dap").session() end, desc = "Session" },
-      { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
-      { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
 
-      { "<leader>dn", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<leader>dd", function() require("dap").continue() end, desc = "Start Debug" },
+      { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+      { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+      { "<leader>ds", function() require("dap").step_over() end, desc = "Step Over" },
       { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
       { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
       { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
       { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
-      { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+
+      { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+
+      { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
+      { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+      { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+      { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+      { "<leader>dS", function() require("dap").session() end, desc = "Session" },
+      { "<leader>dT", function() require("dap").terminate() end, desc = "Terminate" },
     },
     dependencies = {
       {
--- neovim/.config/nvim/lua/plugins/coding/neotest.lua
+++ neovim/.config/nvim/lua/plugins/coding/neotest.lua
@@ -3,10 +3,11 @@ return {
     "nvim-neotest/neotest",
     -- stylua: ignore
     keys = {
-      { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
-      { "<leader>tT", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run All Test Files" },
-      { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
+      { "<leader>tt", function() require("neotest").run.run() end, desc = "Run Nearest" },
       { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last" },
+      { "<leader>ta", function() require("neotest").run.attach() end, desc = "Run attach" },
+      { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
+      { "<leader>tF", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run All Test Files" },
       { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
       { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
       { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
