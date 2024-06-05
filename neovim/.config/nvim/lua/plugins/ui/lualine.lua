0a4b35c1fed603ae238eb496ab777bd533874280
刘军 <liujun@gw.com.cn>
2024-06-05 17:43:15 +0800

    modified:   neovim/.config/nvim/lua/plugins/ui/lualine.lua

--- neovim/.config/nvim/lua/plugins/ui/lualine.lua
+++ neovim/.config/nvim/lua/plugins/ui/lualine.lua
@@ -1,9 +1,28 @@
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
-  opts = {
-    options = {
-      globalstatus = true,
-    },
-  },
+  opts = function()
+    local version = vim.version().minor
+    local function stbufnr()
+      return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+    end
+    local function lsp()
+      if rawget(vim, "lsp") and version >= 10 then
+        for _, client in ipairs(vim.lsp.get_clients()) do
+          if client.attached_buffers[stbufnr()] then
+            return (vim.o.columns > 100 and "   LSP ~ " .. client.name .. " ") or "   LSP "
+          end
+        end
+      end
+      return ""
+    end
+    return {
+      options = {
+        globalstatus = true,
+      },
+      sections = {
+        lualine_x = { lsp, "encoding", "fileformat", "filetype" },
+      },
+    }
+  end,
 }
